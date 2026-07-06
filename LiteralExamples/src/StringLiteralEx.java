18e2a6229b488be6c32a56c11cf15f500e6911c9
Aniket Pandey <aniketpandeyoncoc@gmail.com>
2026-07-06 11:38:18 +0530

    commit collection

---
+++ LiteralExamples/src/StringLiteralEx.java
@@ -0,0 +1,14 @@
+
+public class StringLiteralEx {
+	public static void main(String[] args) {
+		String s="Proiyansh Badaghaiya";
+//		String s='jdnfnsd';//Error
+//		String s=sdkjbds; //Error
+		String ss="123";
+		String sss="33.56";
+		System.out.println(s);
+		System.out.println(ss);
+		System.out.println(sss);
+
+	}
+}
