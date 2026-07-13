de680e52d24289922cea9d17b57defe5b018c83e
Aniket Pandey <aniketpandeyoncoc@gmail.com>
2026-07-13 14:32:14 +0530

    Loop Controls Questions

---
+++ LoopControlsQues/src/Cubes.java
@@ -0,0 +1,11 @@
+
+public class Cubes {
+
+	    public static void main(String[] args) {
+
+	        for(int i = 1; i <= 15; i++) {
+	            System.out.println("Cube of " + i + " = " + (i * i * i));
+	        }
+	    }
+	}
+
