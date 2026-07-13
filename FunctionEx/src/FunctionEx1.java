c6a69906d92b5a5265226a98c8b64819a4904f1a
Aniket Pandey <aniketpandeyoncoc@gmail.com>
2026-07-13 14:26:19 +0530

    Function Examples

---
+++ FunctionEx/src/FunctionEx1.java
@@ -0,0 +1,16 @@
+public class FunctionEx1 {
+ static void kuchbhi() {
+	 System.out.println("Hello yoyo");
+	 System.out.println("Hello Gogo");
+ }
+ 	public static void main(String[]args) {
+ 		show();
+ 		kuchbhi();
+ 		kuchbhi();
+ 		show();
+ 		
+ 	}
+ 	static void show() {
+ 		System.out.println("Hello universe");
+ 	}
+ 	}
