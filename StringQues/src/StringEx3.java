2c8028258d054ebdeeea4a7c18d2bfaff6db11ec
Aniket Pandey <aniketpandeyoncoc@gmail.com>
2026-07-13 15:04:30 +0530

    String Question

---
+++ StringQues/src/StringEx3.java
@@ -0,0 +1,10 @@
+public class StringEx3 {
+	public static void main(String[] args) {
+		String s1=new String("Priyansh");
+		String s2=new String("Yoyo");
+		String s3="Priyansh";
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+	}
+}
---
+++ StringQues/src/StringEx4.java
@@ -0,0 +1,31 @@
+public class StringEx4 {
+	public static void main(String[] args) {
+		String s1="Priyansh Badaghaiya";
+		String s2="Badaghaiya";
+		s1.concat(s2);
+		System.out.println(s1);
+		System.out.println(s1.toUpperCase());
+		System.out.println("Universal".toUpperCase());
+		System.out.println(s1.toLowerCase());
+		//System.out.println(s1[2]);//error
+		System.out.println(s1.charAt(2));
+		System.out.println(s1+s2);
+		System.out.println(s1.concat(s2));
+		System.out.println(s1.length());
+		for(int x=0;x<s1.length();x++)
+			System.out.println(s1.charAt(x));
+		System.out.println(s1.substring(2));
+		System.out.println(s1.substring(2,8));
+		System.out.println(s1.indexOf('h'));
+		System.out.println(s1.indexOf('h',3));
+		System.out.println(s1.indexOf('k'));
+		System.out.println(s1.lastIndexOf('h'));
+		System.out.println(s1.indexOf("Pandey"));
+		System.out.println("  ram          kumar  ".trim());
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.equals("Priyansh Badaghaiya"));
+		System.out.println(s1.equals("Priyansh Badaghaiya"));
+		System.out.println(s1.equalsIgnoreCase("Priyansh Badaghaiya"));
+		System.out.println(s1.equalsIgnoreCase(s2));
+	}
+}
